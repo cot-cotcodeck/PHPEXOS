095f14207c6a7da7e6e62c09369de131e4e5f8b4
Audreymg <maneaudrey.a@gmail.com>
2019-12-03 16:30:55 +0100

    PHPEXOS

---
+++ partie3/ex1/index.php
@@ -0,0 +1,17 @@
+<?php
+$arrayName = array('Months' => 'Janvier',
+'Février',
+'Mars',
+'Avril',
+'Mai',
+'Juin',
+'Juillet'); ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Ex1p3</title>
+  </head>
+  <body>
+  </body>
+</html>
---
+++ partie3/ex2/index.php
@@ -0,0 +1,20 @@
+<?php
+$arrayName = array('Janvier',
+'Février',
+'Mars',
+'Avril',
+'Mai',
+'Juin',
+'Juillet');
+ ?>
+<!DOCTYPE html>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>ex2p3</title>
+  </head>
+  <body>
+    <p>
+    <?php echo $arrayName [5];?></p>
+  </body>
+</html>
